461bab5cf088c02ff4ac8801af68007c1d255b92
Kamil Młodziński <haniakkamil@gmail.com>
2023-11-06 20:18:47 +0100

    adding new task interction

--- src/App.js
+++ src/App.js
@@ -8,64 +8,75 @@ import Container from "./Container";
 import { useState } from "react";
 
 
- 
+
 
 function App() {
-const [hideDone, setHideDone] = useState(false);
-const [tasks, setTasks] = useState([
-  { id: 1, content: "przejsc na reacta", done: false },
-  { id: 2, content: "zjesc obiad", done: true },
-]);
+  const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejsc na reacta", done: false },
+    { id: 2, content: "zjesc obiad", done: true },
+  ]);
 
-const toggleHideDone = () => {
-  setHideDone(hideDone =>!hideDone);
-};
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
 
-const removeTask = (id) => {
-  setTasks(tasks =>tasks.filter(tasks => tasks.id !== id ));
-};
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(tasks => tasks.id !== id));
+  };
 
-const toggleTaskDone = (id) => {
-  setTasks(tasks => tasks.map(task => {
-    if (task.id === id) {
-      return {...task, done: !task.done};
-    }
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
 
-    return task;
-  }));
-}
+      return task;
+    }));
+  }
 
-const setAllDone = () => {
-  setTasks(tasks => tasks.map(task => ({
-... task,
-done: true,
-  })));
-};
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  };
+
+  const  addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.lenght - 1].id + 1 : 1,
+      },
+    ]);
+  };
 
   return (
     <Container>
-    <Header title="Lista zadań"/>
-    <Section title="dodaj nowe zadanie" body={< Form/>} />
-    <Section 
-    title="Lista zadań"
-    body={
-    < Tasks 
-    tasks={tasks} 
-    hideDone={hideDone} 
-    removeTask={removeTask}
-    toggleTaskDone={toggleTaskDone}
-     />
-    }
-    extraHeaderContent={
-    <Buttons 
-    tasks={tasks} 
-    hideDone={hideDone} 
-    toggleHideDone={toggleHideDone}
-    setAllDone={setAllDone} 
-    />
-  }
-    />
-</Container>
+      <Header title="Lista zadań" />
+      <Section title="dodaj nowe zadanie" body={< Form addNewTask={addNewTask} />} />
+      <Section
+        title="Lista zadań"
+        body={
+          < Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
   )
 };
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,34 @@
 
 import "./style.css";
+import { useState } from "react";
 
-const Form = () => (
-    <form className="form">
-    <input className="form__input" name="input" placeholder="Co jest do zrobienia?" autofocus/>
+
+const Form = ({addNewTask}) => {
+
+const [newTaskContent, setNewTaskContent] = useState("");
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
+};
+
+
+    return (
+    <form className="form" onSubmit={onFormSubmit}>
+    <input 
+    value={setNewTaskContent} 
+    className="form__input" 
+    name="input" 
+    placeholder="Co jest do zrobienia?"
+    onChange = {({target}) => setNewTaskContent(target.value)}
+    autofocus/>
     <button className="form__button">Dodaj zadanie</button>
 </form>
-);
+)
+;}
+    
+;
 
 
 export default Form;
