6e7215f2fae4473931afbfec619447106494e0b3
Kamil Młodziński <haniakkamil@gmail.com>
2024-03-02 11:41:43 +0100

    Add local storage

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ import Container from "./Container";
 import { useState } from "react";
 
 
-
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejsc na reacta", done: false },
-    { id: 2, content: "zjesc obiad", done: true },
-  ]);
+  
+  const [tasks, setTasks,] = useState(
+      JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]
+  );
+
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
